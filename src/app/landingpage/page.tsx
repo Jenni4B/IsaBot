5851d658b19992248c2d10cff3b361edd9ef42e8
Jenni4B <jennifer.ea.blocker@gmail.com>
2025-05-20 16:20:05 -0400

    initial

---
+++ src/app/landingpage/page.tsx
@@ -0,0 +1,9 @@
+
+const LandingPage = () => {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold mb-4">Welcome to the Landing Page</h1>
+        <p className="text-lg">This is a simple landing page.</p>
+        </div>
+    );
+}
