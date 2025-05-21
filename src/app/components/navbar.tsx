7b25761b75be47028570cb49dabd0c45a7751804
Jenni4B <jennifer.ea.blocker@gmail.com>
2025-05-21 15:26:39 -0400

    Changed the background :)

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 
 const navItems = [
     { name: "Home", href: "/" },
-    { name: "About", href: "/about" },
     { name: "Contact", href: "/contact" },
-    { name: "Blog", href: "/blog" },
-    { name: "Projects", href: "/projects" },
     ];
+
+
+
 const NavBar: React.FC = () => {
     return (
-        <nav className="bg-gray-800 text-white p-4">
+        <nav className="text-white p-4">
             <div className="container mx-auto flex justify-between items-center">
                 <div className="text-lg font-bold">My Website</div>
                 <ul className="flex space-x-4">
