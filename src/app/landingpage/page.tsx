16c17f583112ad9e0cb4dc0f530d5692b4c1307b
Jenni4B <jennifer.ea.blocker@gmail.com>
2025-05-23 16:49:51 -0400

    Added padding to the landing page and changed the alignment

--- src/app/landingpage/page.tsx
+++ src/app/landingpage/page.tsx
@@ -3,21 +3,23 @@
 const LandingPage = () => {
     return (
         <div className="flex flex-col items-center justify-center flex-1">
-            <h1 className="text-4xl font-bold mb-4">Welcome to IsaBot</h1>
-            <p className="text-lg">
-                Prepare for progress! And make it double!
-                To protect your time from administration,
-                To unite all check-ins in one location!
-            </p>
-            <button
-                className="text-white py-2 px-4 rounded hover:opacity-90 font-sans"
-                style={{ backgroundColor: "var(--color-accent, #D0355B)" }}
-                onClick={() => {
-                    window.location.href = "/dashboard";
-                }}
-            >
-                View Clients
-            </button>
+            <div className="p-8">
+                <h1 className="text-4xl font-bold mb-4">Welcome to IsaBot</h1>
+                <p className="text-lg mb-6">
+                    Prepare for progress! And make it double!
+                    To protect your time from administration,
+                    To unite all check-ins in one location!
+                </p>
+                <button
+                    className="text-white py-2 px-4 rounded hover:opacity-90 font-sans"
+                    style={{ backgroundColor: "var(--color-accent, #D0355B)" }}
+                    onClick={() => {
+                        window.location.href = "/dashboard";
+                    }}
+                >
+                    View Clients
+                </button>
+            </div>
         </div>
     );
 }
