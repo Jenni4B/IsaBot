4fb379ee53028a912090757de3ec6a3418cbc7bb
Jenni4B <jennifer.ea.blocker@gmail.com>
2025-06-12 14:21:51 -0400

    Redownloaded googleapis bc it was being a bit weird. Then I added googledrive.ts under lab folder next to playlab.ts so I can find it easier. I got the secret creds in there so by the next commit I'll have tested the google drive api with some test files

---
+++ src/app/lab/googleDrive.ts
@@ -0,0 +1,46 @@
+import { google } from 'googleapis';
+
+const auth = new google.auth.GoogleAuth({
+    // points to my credentials file
+    keyFile: 'credentials.json',
+
+    // scopes for read-only access to Google Drive
+    scopes: ['https://www.googleapis.com/auth/drive.readonly'],
+});
+
+const drive = google.drive({ version: 'v3', auth });
+
+export async function listFilesInFolder(folderId: string) {
+  const res = await drive.files.list({
+
+    // Use the folderId to filter files in that folder
+    // I might not need this though because in the folder
+    // it's only text files but this is just in case
+    
+    q: `'${folderId}' in parents and mimeType = 'text/plain'`,
+    fields: 'files(id, name)',
+  });
+
+  return res.data.files || [];
+}
+
+export async function downloadFile(fileId: string) {
+  const res = await drive.files.get(
+    {
+      fileId,
+      alt: 'media',
+    },
+    { responseType: 'stream' }
+  );
+
+  return new Promise<string>((resolve, reject) => {
+    let data = '';
+    res.data.on('data', (chunk) => {
+      data += chunk;
+    });
+    res.data.on('end', () => {
+      resolve(data);
+    });
+    res.data.on('error', reject);
+  });
+}
