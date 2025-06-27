b98dbae09d909d89e86f8c566773b3c18f51a498
Jenni4B <jennifer.ea.blocker@gmail.com>
2025-06-27 09:25:44 -0400

    Fixed build error in oopsPage

--- src/app/oopsPage/page.tsx
+++ src/app/oopsPage/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Link from 'next/link';
 
 
-export const OopsPage = () => {
+const OopsPage = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-red-600 mb-4">Oops! Try again later!</h1>
@@ -11,3 +11,5 @@ export const OopsPage = () => {
     </div>
   );
 }
+
+export default OopsPage;
